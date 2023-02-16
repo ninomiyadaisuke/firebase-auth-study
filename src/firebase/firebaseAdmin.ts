8d051886b77095ff6ff416bda4cd42318e8493a5
ninomiyadaisuke <daisukeninomiya0375@gmail.com>
2023-02-16 15:15:21 +0900

    changed environment variables

--- src/firebase/firebaseAdmin.ts
+++ src/firebase/firebaseAdmin.ts
@@ -6,9 +6,9 @@ import admin from 'firebase-admin';
  */
 
 const serviceAccount: admin.ServiceAccount = {
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  clientEmail: process.env.NEXT_PUBLIC_FIREBASE_ADMIN_CLIENT_EMAIL,
-  privateKey: (process.env.NEXT_PUBLIC_FIREBASE_ADMIN_PRIVATE_KEY || '').replace(/\\n/g, '\n'),
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  clientEmail: process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
+  privateKey: (process.env.FIREBASE_ADMIN_PRIVATE_KEY || '').replace(/\\n/g, '\n'),
 };
 
 /**
