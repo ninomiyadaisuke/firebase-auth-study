e43ba1a874c35e2e3d96c5e5909a426c3ef9b7e9
ninomiyadaisuke <daisukeninomiya0375@gmail.com>
2023-02-17 14:48:09 +0900

    fix

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,8 +3,12 @@ import { NextResponse } from 'next/server';
 
 export function middleware(req: NextRequest) {
   const cookie = req.cookies.get('session')?.value;
-  if (!cookie && req.nextUrl.pathname.startsWith('/dashboard')) {
+  if (!cookie) {
     return NextResponse.redirect(new URL('/login', req.url));
   }
   return NextResponse.next();
 }
+
+export const config = {
+  matcher: '/dashboard',
+};
