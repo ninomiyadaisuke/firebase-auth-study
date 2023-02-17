8613205e44e6b652bd9dd48b4874c203e658d1d8
ninomiyadaisuke <daisukeninomiya0375@gmail.com>
2023-02-17 16:22:39 +0900

    fix code

--- src/pages/api/auth/session.ts
+++ src/pages/api/auth/session.ts
@@ -1,9 +1,15 @@
 import type { NextApiHandler } from 'next';
 
+import { firebaseAdmin } from '@/firebase/firebaseAdmin';
 import { assignSession, logIn, signUp } from '@/firebase/utils';
 
 const handler: NextApiHandler = async (req, res) => {
   if (req.method !== 'POST') return res.status(404).send('Not Found');
+
+  const auth = firebaseAdmin.auth();
+  // Tokenの有効期限
+  const expiresIn = 60 * 60 * 24 * 5 * 1000; // 5日
+
   const { email, password, authentication } = req.body;
 
   if (authentication === 'signup') {
@@ -12,7 +18,9 @@ const handler: NextApiHandler = async (req, res) => {
       throw new Error('認証エラー');
     }
     const { idToken } = await response.json();
-    assignSession(res, idToken);
+    const sessionCookie = await auth.createSessionCookie(idToken, { expiresIn });
+    await assignSession(res, sessionCookie, expiresIn);
+    return res.send(JSON.stringify({ status: 'success' }));
   }
 
   if (authentication === 'login') {
@@ -21,10 +29,9 @@ const handler: NextApiHandler = async (req, res) => {
       throw new Error('認証エラー');
     }
     const { idToken } = await response.json();
-    assignSession(res, idToken);
+    await assignSession(res, idToken, expiresIn);
+    return res.send(JSON.stringify({ status: 'success' }));
   }
-
-  res.send(JSON.stringify({ status: 'success' }));
 };
 
 export default handler;
