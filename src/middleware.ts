e8f3972b38a44f09d328800dbb989235b2e97d68
ninomiyadaisuke <daisukeninomiya0375@gmail.com>
2023-02-20 11:01:37 +0900

    save

--- src/middleware.ts
+++ src/middleware.ts
@@ -9,7 +9,7 @@ export function middleware(req: NextRequest) {
       return NextResponse.redirect(new URL('/login', req.url));
     }
   }
-  return NextResponse.next();
+  // return NextResponse.next();
 }
 
 export const config = {
