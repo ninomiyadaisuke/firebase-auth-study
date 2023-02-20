0625b9060ddb79a4b48dd66538dfc5cfbfc1a5ad
ninomiyadaisuke <daisukeninomiya0375@gmail.com>
2023-02-20 10:39:31 +0900

    save

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,16 +2,11 @@ import type { NextRequest } from 'next/server';
 import { NextResponse } from 'next/server';
 
 export async function middleware(req: NextRequest) {
-  const login = () => {
-    if (process.env.NODE_ENV === 'production') {
-      return 'https://firebase-auth-study.vercel.app/login';
-    }
-    return 'http://localhost:3000/login';
-  };
   const cookie = req.cookies.get('session')?.value;
-
-  if (!cookie) {
-    return NextResponse.redirect(new URL(login(), req.url));
+  if (req.nextUrl.pathname.startsWith('/dashboard')) {
+    if (!cookie) {
+      return NextResponse.redirect(new URL('/login', req.url));
+    }
   }
   return NextResponse.next();
 }
