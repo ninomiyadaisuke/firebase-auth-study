b0a4dce03ae9a0519a6b04f4c27858b6b969fd62
ninomiyadaisuke <daisukeninomiya0375@gmail.com>
2023-02-20 10:54:52 +0900

    save

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,7 @@ import type { NextRequest } from 'next/server';
 import { NextResponse } from 'next/server';
 
 export function middleware(req: NextRequest) {
-  const cookie = req.cookies.get('session')?.value;
+  const cookie = req.cookies.get('session');
   if (req.nextUrl.pathname.startsWith('/dashboard')) {
     if (!cookie) {
       return NextResponse.redirect(new URL('/login', req.url));
