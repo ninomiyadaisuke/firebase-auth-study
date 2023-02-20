442ade187903485132d0e229c57097740a0f3be1
ninomiyadaisuke <daisukeninomiya0375@gmail.com>
2023-02-20 10:06:37 +0900

    save

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,16 +1,18 @@
 import type { NextRequest } from 'next/server';
 import { NextResponse } from 'next/server';
 
-const authenticatedPaths: string[] = ['/dashboard'];
-
 export function middleware(req: NextRequest) {
   const url = req.nextUrl.clone();
   const login = `${url.origin}/login`;
-  const cookie = req.cookies.get('session')?.value;
-  if (!cookie) {
-    if (authenticatedPaths.includes(url.pathname)) {
-      return NextResponse.redirect(login);
-    }
-  }
-  return NextResponse.next();
+  // const cookie = req.cookies.get('session')?.value;
+  // if (!cookie) {
+  // if (authenticatedPaths.includes(url.pathname)) {
+  return NextResponse.redirect(login);
+  // }
+  // }
+  // return NextResponse.next();
 }
+
+export const config = {
+  matcher: '/dashboard',
+};
