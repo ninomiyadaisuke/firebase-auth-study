bd0aa2fa065dc6eb3e6d3c5aac3cb5a8483d4baf
ninomiyadaisuke <daisukeninomiya0375@gmail.com>
2023-02-20 10:26:09 +0900

    save

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,16 +1,19 @@
 import type { NextRequest } from 'next/server';
 import { NextResponse } from 'next/server';
 
-export function middleware(req: NextRequest) {
-  const url = req.nextUrl.clone();
-  const login = `${url.origin}/login`;
-  // const cookie = req.cookies.get('session')?.value;
-  // if (!cookie) {
-  // if (authenticatedPaths.includes(url.pathname)) {
-  return NextResponse.redirect(login);
-  // }
-  // }
-  // return NextResponse.next();
+export async function middleware(req: NextRequest) {
+  const login = () => {
+    if (process.env.NODE_ENV === 'production') {
+      return 'https://firebase-auth-study.vercel.app/login';
+    }
+    return 'http://localhost:3000/login';
+  };
+  const cookie = req.cookies.get('session')?.value;
+
+  if (!cookie) {
+    return NextResponse.redirect(new URL(login(), req.url));
+  }
+  return NextResponse.next();
 }
 
 export const config = {
