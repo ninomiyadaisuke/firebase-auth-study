6d949422746dc0a305c9872ca579a42334b9c48e
ninomiyadaisuke <daisukeninomiya0375@gmail.com>
2023-02-20 10:43:01 +0900

    save

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import type { NextRequest } from 'next/server';
 import { NextResponse } from 'next/server';
 
-export async function middleware(req: NextRequest) {
+export function middleware(req: NextRequest) {
   const cookie = req.cookies.get('session')?.value;
   if (req.nextUrl.pathname.startsWith('/dashboard')) {
     if (!cookie) {
